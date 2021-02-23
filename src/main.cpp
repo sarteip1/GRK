22d918a34375e8ce7bff99f31ce53704f0bf1347
Lech Wołowski <lech.wolowski@gmail.com>
2021-02-23 23:33:26 +0100

    physix in works

--- src/main.cpp
+++ src/main.cpp
@@ -23,8 +23,25 @@
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
 #include "Texture.h"
+#include "Physics.h"
 
 float frustumScale = 1.f;
+
+Physics pxScene(9.8);
+
+// fixed timestep for stable and deterministic simulation
+const double physicsStepTime = 1.f / 90.0f;
+double physicsTimeToProcess = 0;
+
+// physical objects
+//PxRigidStatic *planeBody = nullptr;
+//PxMaterial *planeMaterial = nullptr;
+//std::vector<PxRigidDynamic*> asteroidBodies;
+//PxMaterial *asteroidMaterial = nullptr;
+//PxRigidStatic *sphereBody = nullptr;
+//PxMaterial *sphereMaterial = nullptr;
+PxRigidDynamic *shipBody = nullptr;
+PxMaterial* shipMaterial = nullptr;
 
 //programs
 GLuint program;
@@ -63,6 +80,15 @@ glm::vec3 lightPos = glm::vec3(0.0f, 0.0f, 0.0f);
 static const int NUM_ASTEROIDS = 8;
 glm::vec3 asteroidPositions[NUM_ASTEROIDS];
 
+struct Renderable {
+    obj::Model *model;
+    glm::mat4 modelMatrix;
+    GLuint textureId;
+	GLuint textureNormal;
+};
+Renderable* ship;
+    //rendHandle, rendSpheres[3], rendBox, rendGate;
+
 void keyboard(unsigned char key, int x, int y)
 {
 	float angleSpeed = 0.1f;
@@ -71,7 +97,7 @@ void keyboard(unsigned char key, int x, int y)
 	{
 	case 'z': cameraAngle -= angleSpeed; break;
 	case 'x': cameraAngle += angleSpeed; break;
-	case 'w': cameraPos += cameraDir * moveSpeed; break;
+	case 'w': shipBody->addForce(PxVec3(cameraDir.x * -1 * 1000.0f, 0, cameraDir.z * -1 * 1000.0f), PxForceMode::eFORCE, true); break;
 	case 's': cameraPos -= cameraDir * moveSpeed; break;
 	case 'd': cameraPos += glm::cross(cameraDir, glm::vec3(0, 1, 0)) * moveSpeed; break;
 	case 'a': cameraPos -= glm::cross(cameraDir, glm::vec3(0, 1, 0)) * moveSpeed; break;
@@ -119,36 +145,74 @@ void drawObjectTexture(GLuint program, obj::Model *model, glm::mat4 modelMatrix,
 	glUseProgram(0);
 }
 
-unsigned int loadCubemap(std::vector<std::string> faces)
+// unsigned int loadCubemap(std::vector<std::string> faces)
+// {
+// 	unsigned int textureID;
+// 	glGenTextures(1, &textureID);
+// 	glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+
+// 	int width, height, nrChannels;
+// 	for (unsigned int i = 0; i < faces.size(); i++)
+// 	{
+// 		unsigned char* data = stbi_load(faces[i].c_str(), &width, &height, &nrChannels, 0);
+// 		if (data)
+// 		{
+// 			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
+// 				0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data
+// 			);
+// 			stbi_image_free(data);
+// 		}
+// 		else
+// 		{
+// 			std::cout << "Cubemap tex failed to load at path: " << faces[i] << std::endl;
+// 			stbi_image_free(data);
+// 		}
+// 	}
+// 	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+// 	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+// 	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+// 	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+// 	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+// 	return textureID;
+// }
+
+void updateTransforms()
 {
-	unsigned int textureID;
-	glGenTextures(1, &textureID);
-	glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+    // Here we retrieve the current transforms of the objects from the physical simulation.
+    auto actorFlags = PxActorTypeFlag::eRIGID_DYNAMIC | PxActorTypeFlag::eRIGID_STATIC;
+    PxU32 nbActors = pxScene.scene->getNbActors(actorFlags);
+    if (nbActors)
+    {
+        std::vector<PxRigidActor*> actors(nbActors);
+        pxScene.scene->getActors(actorFlags, (PxActor**)&actors[0], nbActors);
+        for (auto actor : actors)
+        {
+            // We use the userData of the objects to set up the model matrices
+            // of proper renderables.
+            if (!actor->userData) continue;
+            Renderable *renderable = (Renderable*)actor->userData;
 
-	int width, height, nrChannels;
-	for (unsigned int i = 0; i < faces.size(); i++)
-	{
-		unsigned char* data = stbi_load(faces[i].c_str(), &width, &height, &nrChannels, 0);
-		if (data)
-		{
-			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
-				0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data
-			);
-			stbi_image_free(data);
-		}
-		else
-		{
-			std::cout << "Cubemap tex failed to load at path: " << faces[i] << std::endl;
-			stbi_image_free(data);
-		}
-	}
-	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+            // get world matrix of the object (actor)
+            PxMat44 transform = actor->getGlobalPose();
+            auto &c0 = transform.column0;
+            auto &c1 = transform.column1;
+            auto &c2 = transform.column2;
+            auto &c3 = transform.column3;
 
-	return textureID;
+            // set up the model matrix used for the rendering
+            glm::mat4 modelMatrix = glm::mat4(
+                c0.x, c0.y, c0.z, c0.w,
+                c1.x, c1.y, c1.z, c1.w,
+                c2.x, c2.y, c2.z, c2.w,
+                c3.x, c3.y, c3.z, c3.w);
+            
+            if (actor->getName() == "asteroid") modelMatrix = modelMatrix * glm::scale(glm::vec3(20.0f));
+            if (actor->userData == ship) modelMatrix = modelMatrix * glm::scale(glm::vec3(0.08f)) * glm::rotate(glm::radians(180.0f), glm::vec3(0, 1, 0)); // IMPORTANT!
+
+            renderable->modelMatrix = modelMatrix;
+        }
+    }
 }
 
 void renderScene()
@@ -167,6 +231,8 @@ void renderScene()
 		* glm::rotate(-cameraAngle + glm::radians(90.0f), glm::vec3(0, 1, 0))
 		* glm::scale(glm::vec3(0.03f));
 
+	ship->modelMatrix = shipModelMatrix;
+
 	glm::mat4 rotate1, rotate2, rotate3, rotate4, moonRotate;
 	rotate1 = glm::rotate((time / 100.0f) * 2 * 3.14159f, glm::vec3(0.0f, 2.0f, 0.0f));
 	rotate2 = glm::rotate((time / 120.0f) * 2 * 3.14159f, glm::vec3(0.0f, 2.0f, 0.0f));
@@ -183,6 +249,8 @@ void renderScene()
 
 	renderSkybox(programSkybox, cameraMatrix, perspectiveMatrix);
 
+	//updateTransforms();
+
 	drawObjectTexture(programTexture, &sphereModel, rotate1 * glm::translate(glm::vec3(0, 0, -10)) * planetScale3 * rotate4, textureVenus, textureMarsNormal);
 	drawObjectTexture(programTexture, &sphereModel, rotate2 * glm::translate(glm::vec3(0, 0, 25)) * planetScale1 * rotate4, textureMars, textureMarsNormal);
 	drawObjectTexture(programTexture, &sphereModel, rotate3 * glm::translate(glm::vec3(0, 0, 20)) * planetScale2 * rotate4, textureEarth, textureEarthNormal);
@@ -195,9 +263,25 @@ void renderScene()
 	}
 
 
-	drawObjectTexture(programTexture, &shipModel, shipModelMatrix, shipTexture, shipTextureNormal);
+	drawObjectTexture(programTexture, ship->model, ship->modelMatrix, ship->textureId, ship->textureNormal);
 
 	glutSwapBuffers();
+}
+
+void initPhysicsScene()
+{
+	shipBody = pxScene.physics->createRigidDynamic(PxTransform(0, 0, 0));
+    shipMaterial = pxScene.physics->createMaterial(0.82f, 0.8f, 0.f);
+    PxShape* shipShape = pxScene.physics->createShape(PxBoxGeometry(1.25f, 0.3f, 1.25f), *shipMaterial);
+    shipBody->attachShape(*shipShape);
+    shipShape->release();
+    shipBody->setMass(5000.0f);
+    shipBody->setMassSpaceInertiaTensor(PxVec3(0.f, 250.0f, 0.f));
+    //spaceshipBody->setRigidDynamicLockFlags(PxRigidDynamicLockFlag::eLOCK_LINEAR_Y | PxRigidDynamicLockFlag::eLOCK_ANGULAR_Z | PxRigidDynamicLockFlag::eLOCK_ANGULAR_X);
+    shipBody->setActorFlag(PxActorFlag::eDISABLE_GRAVITY, true);
+    shipBody->userData = ship;
+
+    pxScene.scene->addActor(*shipBody);
 }
 
 
@@ -225,9 +309,15 @@ void init()
 	shipTextureNormal = Core::LoadTexture("textures/StarSparrow_Normal.png");
 
 	sphereModel = obj::loadModelFromFile("models/sphere.obj");
-	shipModel = obj::loadModelFromFile("models/StarSparrow02.obj");
 
+	shipModel = obj::loadModelFromFile("models/StarSparrow02.obj");
 	shipTexture = Core::LoadTexture("textures/StarSparrow_Blue.png");
+
+	ship = new Renderable();
+	ship->model = &shipModel;
+	ship->textureId = shipTexture;
+	ship->textureNormal = shipTextureNormal;
+
 
 	static const float astRadius = 6.0;
 	for (int i = 0; i < NUM_ASTEROIDS; i++) {
@@ -236,6 +326,7 @@ void init()
 	}
 
 	initSkybox();
+	initPhysicsScene();
 }
 
 void shutdown()
@@ -262,8 +353,8 @@ int main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowPosition(200, 200);
-	glutInitWindowSize(1600, 900);
+	glutInitWindowPosition(0, 0);
+	glutInitWindowSize(2560, 1440);
 	glutCreateWindow("OpenGL Pierwszy Program");
 	glewInit();
 
